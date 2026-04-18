7f3783ef7fcba934123fb569516df9066b59facb
gahbriehel <babatise002@gmail.com>
2026-04-18 18:24:51 +0100

    feat: add NextTopLoader and implement authentication guard for app routes

--- apps/app/src/app/app/layout.tsx
+++ apps/app/src/app/app/layout.tsx
@@ -1,10 +1,19 @@
-import "@/src/styles/globals.css";
+"use client";
 
-export default async function AppLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import "@/src/styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAppSelector } from "../hooks/useAppSelector";
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  const { user } = useAppSelector((state) => state.auth);
+  const router = useRouter();
+  useEffect(() => {
+    if (!user) {
+      router.push("/login");
+    }
+  }, [user, router]);
+
   return (
     <div className="flex h-screen overflow-hidden antialiased bg-gray-50">
       <div className="flex-1 flex flex-col overflow-hidden">
